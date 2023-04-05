3363865329422ca2150d8c85825b5119114055eb
harry liu <harrychinese@gmail.com>
2023-04-06 07:02:02 +0800

    test github connection

--- src/MyREST/Program.cs
+++ src/MyREST/Program.cs
@@ -10,7 +10,7 @@ namespace MyREST
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            //   Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
