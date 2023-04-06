52d29b728d02bbc4b45693d8a548fae1182b5180
harry liu <harrychinese@gmail.com>
2023-04-06 22:06:15 +0800

    delete useless code

--- src/MyREST/Controller.cs
+++ src/MyREST/Controller.cs
@@ -11,17 +11,8 @@ namespace MyREST
     public class Controller : ControllerBase
     {
         private readonly IConfiguration _configuration;
-
-        private GlobalConfig _globalConfig;
-
-        private SystemConfig _systemConfig;
-        private List<DbConfig> _dbConfigs;
-
-        private XmlFileContainer _xmlFileContainer;
-
         private readonly ILogger<Controller> _logger;
         private Engine _engine;
-        private Firewall _firewall;
 
         public Controller(ILogger<Controller> logger, IConfiguration configuration,
             GlobalConfig globalConfig, SystemConfig systemConfig, List<DbConfig> dbConfigs, XmlFileContainer xmlFileContainer, Firewall firewall, Engine engine)
@@ -29,29 +20,7 @@ namespace MyREST
         {
             _logger = logger;
             _configuration = configuration;
-            _globalConfig = globalConfig;
-            _systemConfig = systemConfig;
-            _dbConfigs = dbConfigs;
-            _xmlFileContainer = xmlFileContainer;
-            _firewall = firewall;
             _engine = engine;
-        }
-
-        [HttpGet("/test")]
-        public IEnumerable<dynamic> Get2()
-        {
-            string selectSql = """
-                select actor_id , first_name FirstName, last_name LastName, last_update LastUpdate
-                from actor
-                """;
-            ;
-            string connectionString = "Server=localhost;Port=3306;Database=sakila;Uid=root;Pwd=TOOR;";
-            using (IDbConnection conn = new MySqlConnection(connectionString))
-            {
-                //conn.ExecuteReader(sql).GetSchemaTable()
-                Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
-                return conn.Query(selectSql);
-            }
         }
 
         [HttpPost("/invoke")]
@@ -61,44 +30,6 @@ namespace MyREST
             HttpContext.Features.Get<IHttpConnectionFeature>()?.RemoteIpAddress?.ToString();
 
             SqlResultWrapper result = _engine.process(clientIpAddress, sqlRequestWrapper);
-            return result;
-        }
-
-        private SqlResultWrapper Get223(SqlRequestWrapper sqlRequestWrapper)
-        {
-            string selectSql = """
-                select actor_id , first_name FirstName, last_name LastName, last_update LastUpdate
-                from actor
-                """;
-            ;
-
-            SqlRequest request = sqlRequestWrapper.request;
-            string traceId = request.traceId;
-            SqlContext sqlContext = request.sqlContext;
-            SqlResultWrapper result = new SqlResultWrapper();
-            SqlResponse sqlResponse = new SqlResponse();
-            if (_globalConfig.system.writebackRequest)
-            {
-                result.request = request; //writeback both sqlContext and traceId
-            }
-            else
-            {
-                result.request = new SqlRequest();
-                result.request.traceId = traceId; //just writeback the traceId
-            }
-
-            string connectionString = "Server=localhost;Port=3306;Database=sakila;Uid=root;Pwd=TOOR;";
-            using (IDbConnection conn = new MySqlConnection(connectionString))
-            {
-                Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
-                IEnumerable<dynamic> rows = conn.Query(selectSql);
-                sqlResponse.rowCount = 100;
-                sqlResponse.affectedCount = 200;
-                sqlResponse.returnCode = 0;
-                sqlResponse.errorMessage = "aaaaaaaaaa";
-                sqlResponse.rows = rows;
-            }
-            result.response = sqlResponse;
             return result;
         }
     }
